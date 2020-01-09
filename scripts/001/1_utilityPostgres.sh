246e7ac1ab660a75217d87055628d7e320c16d02
MaurickThom <thomtwd@gmail.com>
2020-01-09 02:19:34 -0500

    Hello world

---
+++ scripts/001/1_utilityPostgres.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Programa para realizar algunas operaciones de utilitarias de Postgres
+echo "Hello world"
+
+# para poder ejecutar este archivo se hace ./1_util..
+# pero si nos sale un error tendremos que cambiarle los permisos 
+# yo le ponde chmod a+x
