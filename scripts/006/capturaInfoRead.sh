e8a9d4a7739aed811e124d802ccc8be1921a05ae
MaurickThom <thomtwd@gmail.com>
2020-01-10 04:25:26 -0500

    read

---
+++ scripts/006/capturaInfoRead.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+OPTION=0
+BACKUP_NAME=""
+
+read -p "Ingresa el opción : " OPTION
+read -p "Ingresa el nombre del backup : " BACKUP_NAME
+
+echo "La opcion es  : $OPTION , El nombre del backup es : $BACKUP_NAME"
+
+
