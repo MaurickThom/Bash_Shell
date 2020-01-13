3501644472d8149f724eabe5268482b5a86bbfab
MaurickThom <thomtwd@gmail.com>
2020-01-12 22:02:13 -0500

    function : install PostgreSQL

---
+++ proyecto-gestion-operaciones-postgres/postgres_manager.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+<< COMMENT
+
+date +%Y%m%d  : lo que se hace con este comando es settear el formato de date a 20200112
+pero en mi caso lo hare con _ para que se pueda diferenciar entre año , mes y dia +%Y_%m_%d
+
+date +%Y_%m_%d
+
+COMMENT
+
+current_date=`date +%Y_%m_%d`
+option=0
+
+
+
+
+install_postgreSQL () {
+    echo -e "\nVerificando instalación de postgres ... "
+    verifyInstallPostgres=$(which psql)
+    if [[ $? -eq 0 ]] ;then # el $? me dice del ultimo comando ejecutado,  0 : se resolvio con exito
+        echo -e "\n Postgres ya se encuentra instalado"
+    else
+        echo -e "\n"
+        read -s -p "Ingrese la contraseña del sudo : " password_sudo
+        read -s -p "Ingrese la contraseña que utilizará en postgres : " password_postgres
+        echo -e "\n"
+        echo $password_sudo | sudo -S apt update # lo que -S es leer el output de echo y colocarlo como input al sudo --stdin
+        echo $password_sudo | sudo -S apt-get -y install postgresql postgresl-contrib# -y:  yes a todas las preguntas
+    fi
+}
