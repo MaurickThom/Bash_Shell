4b5c1ea752b20b6f750fba99d2153d1c2e53c056
MaurickThom <thomtwd@gmail.com>
2020-01-13 18:01:43 -0500

    Habilitacion de servicios

--- proyecto-gestion-operaciones-postgres/postgres_manager.sh
+++ proyecto-gestion-operaciones-postgres/postgres_manager.sh
@@ -14,20 +14,24 @@ option=0
 
 install_postgreSQL () {
     echo -e "\nVerificando instalación de postgres ... "
-    verifyInstallPostgres=$(which psql)
+    verifyInstallPostgres=$(which psql) 
     if [[ $? -eq 0 ]] ;then # el $? me dice del ultimo comando ejecutado,  0 : se resolvio con exito
         echo -e "\n Postgres ya se encuentra instalado"
-        return ;
+    else
+        echo -e "\n"
+        read -s -p "Ingrese la contraseña del sudo : " password_sudo # -s silent
+        read -s -p "Ingrese la contraseña que utilizará en postgres : " password_postgres
+        echo -e "\n"
+        echo $password_sudo | sudo -S apt update # lo que -S es leer el output de echo y colocarlo como input al sudo --stdin
+        echo $password_sudo | sudo -S apt-get -y install postgresql postgresl-contrib # -y:  yes a todas las preguntas
+        # cuando ejecutados algun comando con sudo , por defecto el usuario es root y posteriormente nos pide la contraseña
+        # pero esto podemos cambiarlo a otro usuario que tambien tenga los mismos privilegios que el superusuario 
+        # esto se hace con el -u : sudo -u chavo /comando/de/chavo
+        sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD '{$password_postgres}';"
+
+        # Ahora toca habiliar los servicios de postgres
+        echo $password_sudo | sudo -S systemctl enable postgresql.service
+        echo $password_sudo | sudo -S systemctl start postgresql.service
     fi
-    echo -e "\n"
-    read -s -p "Ingrese la contraseña del sudo : " password_sudo
-    read -s -p "Ingrese la contraseña que utilizará en postgres : " password_postgres
-    echo -e "\n"
-    echo $password_sudo | sudo -S apt update # lo que -S es leer el output de echo y colocarlo como input al sudo --stdin
-    echo $password_sudo | sudo -S apt-get -y install postgresql postgresl-contrib # -y:  yes a todas las preguntas
-    # cuando ejecutados algun comando con sudo , por defecto el usuario es root y posteriormente nos pide la contraseña
-    # pero esto podemos cambiarlo a otro usuario que tambien tenga los mismos privilegios que el superusuario 
-    # esto se hace con el -u : sudo -u chavo /comando/de/chavo
-    sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD '{$password_postgres}';"
-    read -n1 -s -r -p "PRESIONE [ENTER] PARA CONTINUAR ... "
+    read -n1 -s -r -p "PRESIONE [ENTER] PARA CONTINUAR ... " # 
 }
